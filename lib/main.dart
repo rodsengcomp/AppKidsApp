f89e646c212992b4f68003a8a8f876b64cde1833
Rodolfo Romaioli Ribeiro de Jesus <74135777+rodsengcomp@users.noreply.github.com>
2021-06-24 23:02:16 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'splashscreen.dart';
 
+// Bem Vinda Bruna, é uma alegria trabalhar com todos que se empenham em contruir um futuro melhor !!!
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
